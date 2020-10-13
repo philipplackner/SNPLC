2a84814688538ba0074ee7a41d1a23642e3307ee
Philipp <mike.litoris@gmail.com>
2020-10-13 19:36:27 +0200

    Setup Dagger-Hilt

---
+++ app/src/main/java/com/example/snplc/SocialNetworkApplication.kt
@@ -0,0 +1,7 @@
+package com.example.snplc
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SocialNetworkApplication : Application()
---
+++ app/src/main/java/com/example/snplc/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.example.snplc.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.example.snplc.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplicationContext(
+        @ApplicationContext context: Context
+    ) = context
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_image)
+            .error(R.drawable.ic_error)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+}
