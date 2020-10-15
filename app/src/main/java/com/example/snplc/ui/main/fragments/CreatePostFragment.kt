64096f77feec8ce5271298e15c18c2fd6923a407
Philipp <mike.litoris@gmail.com>
2020-10-15 22:33:52 +0200

    Added CreatePostViewModel.kt

--- app/src/main/java/com/example/snplc/ui/main/fragments/CreatePostFragment.kt
+++ app/src/main/java/com/example/snplc/ui/main/fragments/CreatePostFragment.kt
@@ -1,7 +1,38 @@
 package com.example.snplc.ui.main.fragments
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.snplc.R
+import com.example.snplc.other.EventObserver
+import com.example.snplc.ui.main.viewmodels.CreatePostViewModel
+import com.example.snplc.ui.snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_create_post.*
 
+@AndroidEntryPoint
 class CreatePostFragment : Fragment(R.layout.fragment_create_post) {
+
+    private val viewModel: CreatePostViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeToObservers()
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.createPostStatus.observe(viewLifecycleOwner, EventObserver(
+            onError = {
+                createPostProgressBar.isVisible = false
+                snackbar(it)
+            },
+            onLoading = { createPostProgressBar.isVisible = true }
+        ) {
+            createPostProgressBar.isVisible = false
+            findNavController().popBackStack()
+        })
+    }
 }
---
+++ app/src/main/java/com/example/snplc/ui/main/viewmodels/CreatePostViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.snplc.ui.main.viewmodels
+
+import android.content.Context
+import android.net.Uri
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.snplc.R
+import com.example.snplc.other.Event
+import com.example.snplc.other.Resource
+import com.example.snplc.repositories.MainRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class CreatePostViewModel @ViewModelInject constructor(
+    private val repository: MainRepository,
+    private val applicationContext: Context,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Main
+) : ViewModel() {
+
+    private val _createPostStatus = MutableLiveData<Event<Resource<Any>>>()
+    val createPostStatus: LiveData<Event<Resource<Any>>> = _createPostStatus
+
+    fun createPost(imageUri: Uri, text: String) {
+        if(text.isEmpty()) {
+            val error = applicationContext.getString(R.string.error_input_empty)
+            _createPostStatus.postValue(Event(Resource.Error(error)))
+        } else {
+            _createPostStatus.postValue(Event(Resource.Loading()))
+            viewModelScope.launch(dispatcher) {
+                val result = repository.createPost(imageUri, text)
+                _createPostStatus.postValue(Event(result))
+            }
+        }
+    }
+}
