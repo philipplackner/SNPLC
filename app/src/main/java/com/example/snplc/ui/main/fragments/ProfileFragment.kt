a49d83173c64aaeba78060729a973809b266f607
Philipp <mike.litoris@gmail.com>
2020-10-31 00:44:42 +0100

    Added ProfileFragment.kt

--- app/src/main/java/com/example/snplc/ui/main/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/snplc/ui/main/fragments/ProfileFragment.kt
@@ -1,7 +1,69 @@
 package com.example.snplc.ui.main.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.snplc.R
+import com.example.snplc.other.EventObserver
+import com.example.snplc.ui.main.viewmodels.BasePostViewModel
+import com.example.snplc.ui.main.viewmodels.ProfileViewModel
+import com.example.snplc.ui.snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_profile.*
 
-class ProfileFragment : Fragment(R.layout.fragment_profile) {
+@AndroidEntryPoint
+open class ProfileFragment : BasePostFragment(R.layout.fragment_profile) {
+
+    override val postProgressBar: ProgressBar
+        get() = profilePostsProgressBar
+    override val basePostViewModel: BasePostViewModel
+        get() {
+            val vm: ProfileViewModel by viewModels()
+            return vm
+        }
+
+    protected val viewModel: ProfileViewModel
+        get() = basePostViewModel as ProfileViewModel
+
+    protected open val uid: String
+        get() = FirebaseAuth.getInstance().uid!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        subscribeToObservers()
+
+        btnToggleFollow.isVisible = false
+        viewModel.loadProfile(uid)
+    }
+
+    private fun setupRecyclerView() = rvPosts.apply {
+        adapter = postAdapter
+        itemAnimator = null
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.profileMeta.observe(viewLifecycleOwner, EventObserver(
+            onError = {
+                profileMetaProgressBar.isVisible = false
+                snackbar(it)
+            },
+            onLoading = { profileMetaProgressBar.isVisible = true }
+        ) { user ->
+            profileMetaProgressBar.isVisible = false
+            tvUsername.text = user.username
+            tvProfileDescription.text = if(user.description.isEmpty()) {
+                requireContext().getString(R.string.no_description)
+            } else user.description
+            glide.load(user.profilePictureUrl).into(ivProfileImage)
+        })
+    }
 }
