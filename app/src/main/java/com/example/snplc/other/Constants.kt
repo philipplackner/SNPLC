cb52fa22d00ba17d6e6d0bf925c7d3754d805229
Philipp <mike.litoris@gmail.com>
2020-11-03 20:42:35 +0100

    Implemented SearchFragment.kt

--- app/src/main/java/com/example/snplc/other/Constants.kt
+++ app/src/main/java/com/example/snplc/other/Constants.kt
@@ -7,4 +7,6 @@ object Constants {
     const val MIN_USERNAME_LENGTH = 3
     const val MAX_USERNAME_LENGTH = 8
     const val MIN_PASSWORD_LENGTH = 8
+
+    const val SEARCH_TIME_DELAY = 500L
 }
--- app/src/main/java/com/example/snplc/ui/main/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/snplc/ui/main/fragments/SearchFragment.kt
@@ -1,7 +1,77 @@
 package com.example.snplc.ui.main.fragments
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.snplc.R
+import com.example.snplc.adapters.UserAdapter
+import com.example.snplc.other.Constants.SEARCH_TIME_DELAY
+import com.example.snplc.other.EventObserver
+import com.example.snplc.ui.main.viewmodels.SearchViewModel
+import com.example.snplc.ui.snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_search.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SearchFragment : Fragment(R.layout.fragment_search) {
+
+    @Inject
+    lateinit var userAdapter: UserAdapter
+
+    private val viewModel: SearchViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        subscribeToObservers()
+
+        var job: Job? = null
+        etSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = lifecycleScope.launch {
+                delay(SEARCH_TIME_DELAY)
+                editable?.let {
+                    viewModel.searchUser(it.toString())
+                }
+            }
+        }
+
+        userAdapter.setOnUserClickListener { user ->
+            findNavController()
+                .navigate(
+                    SearchFragmentDirections.globalActionToOthersProfileFragment(user.uid)
+                )
+        }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.searchResults.observe(viewLifecycleOwner, EventObserver(
+            onError = {
+                searchProgressBar.isVisible = false
+                snackbar(it)
+            },
+            onLoading = {
+                searchProgressBar.isVisible = true
+            }
+        ) { users ->
+            searchProgressBar.isVisible = false
+            userAdapter.users = users
+        })
+    }
+
+    private fun setupRecyclerView() = rvSearchResults.apply {
+        layoutManager = LinearLayoutManager(requireContext())
+        adapter = userAdapter
+        itemAnimator = null
+    }
 }
